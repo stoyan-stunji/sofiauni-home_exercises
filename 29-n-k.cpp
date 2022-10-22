22de6eb5c721577d991080e42d9fd6414cb40afa
Stoyan Ivanov <115835362+stoyan-stunji@users.noreply.github.com>
2022-10-22 18:54:35 +0300

    Create 29-n-k.cpp

---
+++ 29-n-k.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n = 0;
+    cin >> n;
+    int k = 0;
+    cin >> k;
+
+    cout << n - k << " | " << n + k << endl;
+      
+    return 0;
+}
