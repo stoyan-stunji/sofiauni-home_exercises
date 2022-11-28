2c3edddb80bdeb74bfb6992ab567d70e714bde07
Stoyan Ivanov <115835362+stoyan-stunji@users.noreply.github.com>
2022-11-28 19:44:46 +0200

    Create 45-add-and-sort-array.cpp

---
+++ 45-add-and-sort-array.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+
+using namespace std;
+
+const int MAX_SIZE = 100;
+
+void sortArray(int array[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        int minIndex = i;
+        for (int j = i + 1; j < size + 1; j++)
+        {
+            if (array[j] < array[minIndex])
+            {
+                minIndex = j;
+            }
+        }
+        int temp = array[i];
+        array[i] = array[minIndex];
+        array[minIndex] = temp;
+    }
+}
+
+void insertElement(int array[], int size, int element)
+{
+    int i = 0;
+    for (i = 0; i < size; i++)
+    {
+        cin >> array[i];
+    }
+
+    array[i] = element;
+
+    sortArray(array, size);
+}
+
+void printArray(int array[], int size)
+{
+    for (int i = 0; i < size + 1; i++)
+    {
+        cout << array[i] << " ";
+    }
+}
+
+int main()
+{
+    int array[MAX_SIZE];
+    int size = 0;
+    int element = 0;
+
+    cout << "size ";
+    cin >> size;
+
+    cout << "element ";
+    cin >> element;
+
+    cout << "array ";
+    insertElement(array, size, element);
+    printArray(array, size);
+}
