3671189ecb65fe3c93ac0bbde33fe4b09d4f3bdc
Stoyan Ivanov <115835362+stoyan-stunji@users.noreply.github.com>
2022-11-29 17:00:08 +0200

    Update 00-important-code.cpp

--- 00-important-code.cpp
+++ 00-important-code.cpp
@@ -15,7 +15,7 @@ void sortArray(int array[], int size)
     for (int i = 0; i < size; i++)
     {
         int minIndex = i;
-        for (int j = i + 1; j < size + 1; j++)
+        for (int j = i + 1; j < size; j++)
         {
             if (array[j] < array[minIndex])
             {
